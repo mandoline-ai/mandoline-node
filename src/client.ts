c9d0d6ab7e49bfc9e7cd35aa05cc803024c56100
kmckiern <keri.a.mckiernan@gmail.com>
2025-07-26 11:34:13 -0700

    Make the no API key error more helpful

--- src/client.ts
+++ src/client.ts
@@ -66,7 +66,7 @@ export class Mandoline {
   protected getAuthHeader(): Headers {
     if (!this.apiKey) {
       throw new Error(
-        "API key not provided and MANDOLINE_API_KEY environment variable is not set."
+        "Mandoline API key required. Set MANDOLINE_API_KEY environment variable or create one at https://mandoline.ai/account"
       );
     }
     return { "X-API-KEY": this.apiKey };
